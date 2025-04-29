cf8a92d5a9ec97843d675eca17183ff5d6ec6308
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2025-04-29 16:08:02 -0500

    Add Users Controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('users')
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UsersService) { }
+}
