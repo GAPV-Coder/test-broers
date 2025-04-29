3b496dcb0b2349d0d7a625a0ec432b71dc708f56
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2025-04-29 16:46:35 -0500

    Add User Module

---
+++ src/users/users.module.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { UserController } from './users.controller';
+import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ],
+    controllers: [UserController],
+    providers: [UsersService],
+    exports: [UsersService],
+})
+
+export class UsersModule { }
