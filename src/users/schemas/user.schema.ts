32036035e995df0bb4e94cee4563eb92630d5421
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2025-04-29 12:50:11 -0500

    Create user Schema

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+
+/* eslint-disable prettier/prettier */
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose'
+
+@Schema({ timestamps: true })
+export class User extends Document {
+    @Prop({ required: true })
+    fullname: string;
+
+    @Prop({ required: true, unique: true })
+    email: string;
+
+    @Prop({ required: true })
+    password: string;
+
+    @Prop({ default: true })
+    isActive: boolean;
+
+    @Prop({ default: null })
+    resetPasswordToken?: string;
+
+    @Prop({ default: null })
+    resetPasswordExpires?: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User); 
