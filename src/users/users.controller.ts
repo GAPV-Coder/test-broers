9099835c6c7b857ad7193fca69e5826499d6e505
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2025-04-29 16:37:44 -0500

    Update User Controller with CRUD methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,11 +9,47 @@ import {
     Delete,
     UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
-import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
 @ApiTags('users')
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: UsersService) { }
+
+    @Post()
+    create(@Body() CreateUserDto: CreateUserDto) {
+        return this.userService.create(CreateUserDto);
+    }
+
+    @Get()
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    findAll() {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    findOne(@Param('id') id: string) {
+        return this.userService.findOne(id);
+    }
+
+    @Patch(':id')
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    update(@Param('id') id: string, @Body() UpdateUserDto: UpdateUserDto) {
+        return this.userService.update(id, UpdateUserDto);
+    }
+
+    @Delete(':id')
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    remove(@Param('id') id: string) {
+        return this.userService.remove(id);
+    }
 }
